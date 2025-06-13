878fd9c91d1ae86156998e9ddedce53096b66f9d
moises-herrera <moisesherrera037@gmail.com>
2025-06-13 16:04:56 -0500

    refactor: use network load balancer for backend pods

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -90,6 +90,7 @@ module "eks" {
   }
 }
 
+# EBS CSI Driver
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
@@ -105,6 +106,13 @@ module "irsa-ebs-csi" {
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
+# AWS Load Balancer Controller
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name_prefix = "AWSLoadBalancerController"
+  description = "EKS AWS Load Balancer Controller policy"
+  policy      = file("${path.module}/../iam/AWSLoadBalancerController.json")
+}
+
 module "irsa-aws-load-balancer-controller" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.58.0"
@@ -114,13 +122,6 @@ module "irsa-aws-load-balancer-controller" {
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [aws_iam_policy.aws_load_balancer_controller.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
-}
-
-# AWS Load Balancer Controller
-resource "aws_iam_policy" "aws_load_balancer_controller" {
-  name_prefix = "AWSLoadBalancerController"
-  description = "EKS AWS Load Balancer Controller policy"
-  policy      = file("${path.module}/../iam/AWSLoadBalancerController.json")
 }
 
 resource "kubernetes_service_account" "aws_load_balancer_controller" {
@@ -163,6 +164,7 @@ resource "helm_release" "aws_load_balancer_controller" {
   ]
 }
 
+# IAM Role for Backend Service Account to access Secrets Manager
 resource "aws_iam_role" "cloudnest_backend_secrets_role" {
   name = "cloudnest-backend-secrets-role"
 
